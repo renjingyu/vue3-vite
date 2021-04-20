e568b192294a2ddbb02b7f0da7f44416db6332f1
renjingyu2 <497663568@11.com>
2021-04-20 18:28:00 +0800

    feat(components): add to GitHub link in Header component

---
+++ src/utils/test.ts
@@ -0,0 +1,2 @@
+const a = 112
+console.log('%c 🍞 a: ', 'font-size:20px;background-color: #2EAFB0;color:#fff;', a)
---
+++ src/utils/test2.ts
@@ -0,0 +1,2 @@
+const b = 11223
+console.log('%c 🍖 b: ', 'font-size:20px;background-color: #4b4b4b;color:#fff;', b)
